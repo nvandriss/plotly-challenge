4dc610b00228383fb5a9cc510610a314ca931f4a
Nicole Van Driss <nvandriss@gmail.com>
2022-01-05 15:05:27 -0500

    Update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -18,5 +18,12 @@ Object.entries(idmetadata[0]).forEach(item=>
    {
       panelDisplay.append("p").text(`${item[0]}: ${item[1]}`)
    });
+}
+//Arrays
+const sample_values = Object.keys(data.sample_values);
+const sample_values = Object.values(data.samples.otu_ids)
+const otu_ids = Object.values(data.samples.otu_ids)
+const otu_labels = Object.values(data.samples.otu_labels)
 
 // Bar Chart
+
