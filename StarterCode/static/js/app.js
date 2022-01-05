ab10dad1fd9049dc3891b4ace050f70cc09411b4
Nicole Van Driss <nvandriss@gmail.com>
2022-01-05 15:54:42 -0500

    Update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -4,26 +4,63 @@ async function main() {
     const data = await response.json();
     console.log(data);
 }
-//Dropdown
-function optionChanged(selectedID){
-    console.log(selectedID);
 
-const idmetadata = data.metadata.filter(item=> (item.id == selectedID));
-
-console.log(idmetadata);
-    
-const panelDisplay = d3.select("#sample-metadata");
-panelDisplay.html("");
-Object.entries(idmetadata[0]).forEach(item=> 
-   {
-      panelDisplay.append("p").text(`${item[0]}: ${item[1]}`)
-   });
-}
+function buildCharts(sample_values) {
 //Arrays
 const sample_values = Object.keys(data.sample_values);
-const sample_values = Object.values(data.samples.otu_ids)
 const otu_ids = Object.values(data.samples.otu_ids)
 const otu_labels = Object.values(data.samples.otu_labels)
 
 // Bar Chart
+let bubbleLayout = {
+    margin: { t: 0 },
+    hovermode: "closests",
+    xaxis: { title: "OTU ID"}
+  }
 
+  let bubbleData = [
+    {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: "Earth"
+      }
+    }
+  ]
+
+  Plotly.plot("bubble", bubbleData, bubbleLayout);
+  let pieData = [
+    {
+      values: sample_values.slice(0, 10),
+      labels: otu_ids.slice(0, 10),
+      hovertext: otu_labels.slice(0, 10),
+      hoverinfo: "hovertext",
+      type: "pie"
+    }
+  ];
+  
+  let pieLayout = {
+    margin: { t: 0, l: 0 }
+  };
+
+  Plotly.plot("pie", pieData, pieLayout)
+}
+
+function init() {
+
+}
+const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+
+
+function optionChanged(newSample) {
+  buildCharts(newSample);
+  buildMetadata(newSample);
+}
+
+main();
